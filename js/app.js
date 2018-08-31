afe15e8a645977b37740dd6ff959ffb8cdeb041c
BritishPandaChick <sbartley1080@gmail.com>
2018-08-31 18:16:41 -0400

    Add update method in app.js. Add victory property to Hero class. Add id variable to engine.js file. Create if statement to call cancelAnimationFrame.

--- js/app.js
+++ js/app.js
@@ -52,9 +52,10 @@ class Hero {
     this.step = 101;
     this.jump = 83;
     this.startX = this.step * 2;
-    this.startY = (this.jump * 5) - 20;
+    this.startY = (this.jump * 4) + 55;
     this.x = this.startX;
     this.y = this.startY;
+    this.victory = false;
   }
 
   //Draw hero sprite on the x and y coord positions
@@ -90,20 +91,36 @@ class Hero {
         break;
     }
   }
-}
-    //methods
-      //update position
-        //check collision here
-        //did player x and y collide with enemy ?
-      //check here
-        //did player x and y reach final tile?
-      //Render
-        //draw player sprite on current x and y coord position
-      //Handle keyboard input
-        //update player's x and y property according to input
-      //Reset Hero
-        //set x and y to starting x and y
 
+  update() {
+    //check here
+    for (let enemy of allEnemies) {
+      //did player x and y collide with enemy ?
+      if (this.y === enemy.y && (enemy.x + enemy.step/2 > this.x && enemy.x < this.x + this.step/2)) {
+        this.reset();
+      }
+    }
+    //did player x and y reach final tile?
+    if (this.y === 55) {
+      this.victory = true;
+    }
+  }
+  //Render
+    //draw player sprite on current x and y coord position
+  //Handle keyboard input
+    //update player's x and y property according to input
+  //Reset Hero
+  reset() {
+    //set x and y to starting x and y
+    this.y = this.startY;
+    this.x = this.startX;
+  }
+}
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
 //New Hero Object
 const player = new Hero();
 //Init allEnemies array
@@ -114,11 +131,6 @@ const allEnemies = [];
 
 //For each enemy create and push new enemy object into above array
 allEnemies.push(bug1, bug2, bug3);
-console.log(allEnemies);
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 
 
--- js/engine.js
+++ js/engine.js
@@ -23,6 +23,7 @@ var Engine = (function(global) {
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         lastTime;
+        id;
 
     canvas.width = 505;
     canvas.height = 606;
@@ -55,7 +56,12 @@ var Engine = (function(global) {
         /* Use the browser's requestAnimationFrame function to call this
          * function again as soon as the browser is able to draw another frame.
          */
-        win.requestAnimationFrame(main);
+         if (player.victory === true) {
+           win.cancelAnimationFrame(id);
+         } else {
+           id = win.requestAnimationFrame(main);
+         }
+
     }
 
     /* This function does some initial setup that should only occur once,
@@ -93,7 +99,7 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
-        //player.update();
+        player.update();
     }
 
     /* This function initially draws the "game level", it will then call
